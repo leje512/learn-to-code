7ef364f7e3e32313f75b3d2a1e6fe309f8908e17
jelllea <lea.jell@hs-augsburg.de>
2022-10-03 18:16:24 +0200

    feat: execute code and transform with babel/standalone

--- codingtutorial/src/CodeEditor.svelte
+++ codingtutorial/src/CodeEditor.svelte
@@ -1,22 +1,38 @@
+
 <script>
     import { onMount } from 'svelte';
+    import { transform } from '@babel/standalone'
+
     let code = "";
-    function getCode() {
-      console.log(code)
-    }
     onMount(() => {
         const el = document.getElementById("codeeditor").addEventListener("codeedited", (e) => code = e.detail); // TODO: get rid of error here via typescript?: should be customevent not event
     })
+
+    function logCode() {
+      console.log(code)
+    }
+
+    function runCode() {
+        const transformedCode = transform(code, { presets: ["env"] }).code;
+        const scriptElement = document.createElement('script')
+        scriptElement.text = transformedCode
+        document.getElementById("codeeditor").appendChild(scriptElement)
+    }
 </script>
 
 <div id="codeeditor">
-    <script src="codemirror.bundle.js"></script>
-    <button on:click={getCode}>log code</button>
+    <script type="module" src="/codemirror.bundle.js"></script>
+    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+    <button on:click={logCode}>log code</button>
+    <button on:click={runCode}>run code</button>
 </div>
 
 <style>
 #codeeditor {
     width: 50vw;
-    min-height: 30vh;
+}
+#codeeditor .cm-editor{
+    width: 100%;
+    height: 30vh;
 }
 </style>
