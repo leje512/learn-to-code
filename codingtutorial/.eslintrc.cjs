0b33f21382e3fc3c9ce6fab42729b2457ba92f74
jelllea <lea.jell@hs-augsburg.de>
2022-10-22 15:19:59 +0200

    chore: add eslint

---
+++ codingtutorial/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["google"],
+  plugins: ["svelte3"],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  ignorePatterns: ["/node_modules", "/dist"],
+  rules: {
+    "max-len": "off",
+    "require-jsdoc": "off",
+  },
+};
