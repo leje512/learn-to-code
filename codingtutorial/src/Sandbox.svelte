75e01f2df820df442a7beacfdacb54e770f68b43
jelllea <lea.jell@hs-augsburg.de>
2022-10-25 15:21:55 +0200

    feat: create experimental iframe

--- codingtutorial/src/Sandbox.svelte
+++ codingtutorial/src/Sandbox.svelte
@@ -12,6 +12,13 @@
     const el = document
       .getElementById("codeeditor")
       .addEventListener("codeedited", (e) => (code = e.detail)); // TODO: get rid of error here via typescript?: should be customevent not event
+    const frame = document.getElementsByTagName("iframe")[0];
+    frame.addEventListener("eval", (event) => {
+      const code = event.detail;
+      console.log("this", this); // this is undefined here
+      console.log("window", window); // same as before
+      Function(code)(); // so this will probably still run in the same context
+    });
   });
 
   function logCode() {
@@ -26,7 +33,15 @@
   }
 
   function run() {
-    Function(code)();
+    const transformedCode = transform(code, { presets: ["env"] }).code;
+    const evaluateCode = new CustomEvent("eval", {
+      detail: transformedCode,
+    });
+    const frame = document.getElementsByTagName("iframe")[0];
+    frame.dispatchEvent(evaluateCode);
+    console.log("this", this); // button
+    console.log("sframe", frame);
+    console.log("swindow", window); // window context
   }
 </script>
 
@@ -34,5 +49,15 @@
   <button on:click={logCode}>log code</button>
   <button on:click={compileCode}>compile code</button>
   <button on:click={run}>run</button>
+  <iframe
+    sandbox="allow-scripts allow-same-origin"
+    title="iframe to compile code safely"
+  />
   <CodeEditor />
 </div>
+
+<style>
+  iframe {
+    display: none;
+  }
+</style>
