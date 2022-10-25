823eda79ee9a05113d6c42d1e5f016196d1d73cd
jelllea <lea.jell@hs-augsburg.de>
2022-10-25 20:19:18 +0200

    feat: catch consolelogs

--- codingtutorial/src/Sandbox.svelte
+++ codingtutorial/src/Sandbox.svelte
@@ -7,32 +7,40 @@
   const dispatch = createEventDispatcher();
 
   let code;
+  let consoleCode;
 
   onMount(() => {
+    // eventListener to get editor content
     const el = document
       .getElementById("codeeditor")
       .addEventListener("codeedited", (e) => (code = e.detail)); // TODO: get rid of error here via typescript?: should be customevent not event
+
+    // override console.log to show message in div
+    const consoleLog = console.log;
+    console.log = function (msg) {
+      consoleLog.apply(console, arguments);
+      consoleCode += msg;
+    };
   });
 
   function logCode() {
     console.log(code);
   }
 
-  function compileCode() {
+  function run() {
     const transformedCode = transform(code, { presets: ["env"] }).code;
     dispatch("code", {
       text: transformedCode,
     });
-  }
-
-  function run() {
-    Function(code)();
+    consoleCode = "";
   }
 </script>
 
 <div id="sandbox">
   <button on:click={logCode}>log code</button>
-  <button on:click={compileCode}>compile code</button>
-  <button on:click={run}>run</button>
+  <button on:click={run}>run code</button>
   <CodeEditor />
+  <div id="console">
+    {consoleCode}
+  </div>
 </div>
