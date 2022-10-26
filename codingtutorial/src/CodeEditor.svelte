84b0c821f936eafbac25732fd30abfea68256067
Lea <lea.jell@peerigon.com>
2022-10-26 14:49:18 +0200

    refactor: move codemirror to editor component

--- codingtutorial/src/CodeEditor.svelte
+++ codingtutorial/src/CodeEditor.svelte
@@ -1,7 +1,34 @@
+<script>
+import { EditorView, basicSetup } from "codemirror";
+import { javascript, esLint } from "@codemirror/lang-javascript";
+import { linter, lintGutter } from "@codemirror/lint";
+
+// TODO: code from here? https://codesandbox.io/s/f6nb0?file=/src/index.js:236-253
+import Linter from "eslint4b-prebuilt";
+import { onMount } from "svelte"
+
+    onMount(() => {
+        const editor = new EditorView({
+        doc: "function sum(a, b) {\n//write your code here\n}\n\n\n\n\n", //TODO: multiple lines at creation
+        extensions: [
+            basicSetup,
+            javascript(),
+            lintGutter(),
+            // @ts-ignore: ts2350
+            linter(esLint(new Linter())),
+            EditorView.updateListener.of((update) => {
+            const codeedited = new CustomEvent("codeedited", {
+                detail: update.state.doc.toString(),
+            });
+            document.getElementById("codeeditor").dispatchEvent(codeedited);
+            }),
+        ],
+        parent: document.getElementById("codeeditor"),
+        });
+    });
+</script>
 
 <div id="codeeditor">
-    <script type="module" src="/codemirror.bundle.js"></script>
-    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
 </div>
 
 <style>
