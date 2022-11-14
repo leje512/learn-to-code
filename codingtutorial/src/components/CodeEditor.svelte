b2f47afa828cee9ed21453183720a9cd3c87362c
jelllea <lea.jell@hs-augsburg.de>
2022-11-14 19:44:37 +0100

    Revert "feat: add customlinter"
    This reverts commit b82510dc008a787657970f871fc888d333b2471b.

--- codingtutorial/src/components/CodeEditor.svelte
+++ codingtutorial/src/components/CodeEditor.svelte
@@ -3,32 +3,10 @@
   import { EditorView, basicSetup } from "codemirror";
   import { javascript, esLint } from "@codemirror/lang-javascript";
   import { linter, lintGutter } from "@codemirror/lint";
-  import { syntaxTree } from "@codemirror/language";
   import Linter from "eslint4b-prebuilt"; // TODO: cite code?? https://codesandbox.io/s/f6nb0?file=/src/index.js:236-253
 
   const dispatch = createEventDispatcher();
   export let initialcode = "";
-
-  const customLinter = linter((view) => {
-    let diagnostics = [];
-    syntaxTree(view.state)
-      .cursor()
-      .iterate((node) => {
-        if (
-          node.name == "ExpressionStatement" &&
-          node.node.parent.name == "Script"
-        ) {
-          diagnostics.push({
-            from: node.from,
-            to: node.to,
-            severity: "warning",
-            message:
-              "Achte darauf, if-else richtig zu verwenden. Der Code innerhalb der if-Anweisung wird ausgeführt, wenn die Bedingung true ergibt. Der Code innerhalb von else wird ausgeführt, wenn die Kondition false ist.",
-          });
-        }
-      });
-    return diagnostics;
-  });
 
   onMount(() => {
     new EditorView({
@@ -38,7 +16,7 @@
         javascript(),
         lintGutter(),
         // @ts-ignore: ts2350
-        customLinter,
+        linter(esLint(new Linter())),
         EditorView.updateListener.of((update) => {
           dispatch("edited", {
             text: update.state.doc.toString(),
