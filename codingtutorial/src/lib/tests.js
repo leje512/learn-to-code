62e74c3a8609ade0f5ce76c3b780d4aaffd7d174
jelllea <lea.jell@hs-augsburg.de>
2022-12-01 18:55:16 +0100

    feat: test case if-else

--- codingtutorial/src/lib/tests.js
+++ codingtutorial/src/lib/tests.js
@@ -2,6 +2,8 @@ export const runUnitTest = (title, code) => {
   switch (title) {
     case "functionSum":
       return checkFunctionSum(code)
+    case "if-else":
+      return checkIfElse(code)
   }
 }
 
@@ -13,6 +15,48 @@ function checkFunctionSum(code) {
     assert.equal(functionToTest(), 3, "test failed")
     return true
   } catch (error) {
+    return false
+  }
+}
+
+function checkIfElse(code) {
+  let consoleCode = ""
+  const consoleLog = console.log
+  console.log = function (msg) {
+    consoleLog.apply(console, arguments)
+    consoleCode = `${consoleCode}${msg}\n`
+  }
+  const testCases = [
+    {
+      variableName: "punkte",
+      value: 4,
+      consoleResult: "Leider durchgefallen.\nAuf Wiedersehen.\n",
+    },
+    {
+      variableName: "punkte",
+      value: 5,
+      consoleResult: "Bestanden.\nAuf Wiedersehen.\n",
+    },
+    {
+      variableName: "punkte",
+      value: 6,
+      consoleResult: "Bestanden.\nAuf Wiedersehen.\n",
+    },
+  ]
+  try {
+    code = code.replace("let punkte = 10;", "")
+    const success = testCases.map(({ value, consoleResult }) => {
+      const testCode = `const punkte = ${value};` + code
+      Function(testCode)()
+      assert.equal(consoleResult, consoleCode, "test failed")
+      consoleCode = ""
+      return true
+    })
+    if (success.every((el) => el === true)) {
+      return true
+    }
+  } catch (error) {
+    console.log("checkIfElse error", error)
     return false
   }
 }
