f76a4940af90c2458a344c43eb0757e2e5c80e12
jelllea <lea.jell@hs-augsburg.de>
2022-12-11 15:47:31 +0100

    test: add test errorSwitchedCompareSymbol, errorMissingIfElse, errorSemicolonAfterIfCondition

--- codingtutorial/src/lib/errorMessages.test.js
+++ codingtutorial/src/lib/errorMessages.test.js
@@ -1,6 +1,10 @@
 import { assert } from "chai"
-import { getDiagnostics } from "./astlint.js"
+import { getDiagnostics, clearLintDiagnostics } from "./astlint.js"
 import errorMessages from "./errorMessages.js"
+
+beforeEach(() => {
+  clearLintDiagnostics()
+})
 
 describe("errorMessages work as expected", () => {
   describe("no misconception should return no errors", () => {
@@ -9,6 +13,7 @@ describe("errorMessages work as expected", () => {
       assert.deepEqual(getDiagnostics(misconceptions, ""), [])
     })
   })
+
   describe("errorConsoleLogNotInBody", () => {
     const misconceptions = [
       {
@@ -24,7 +29,7 @@ describe("errorMessages work as expected", () => {
     ]
     it("with error", () => {
       const code = `
-if (true) {
+if (2 > 5) {
   console.log("Text")
 }`
       const errors = getDiagnostics(misconceptions, code)
@@ -36,27 +41,127 @@ if (true) {
       ])
     })
     it("no error", () => {
-      const misconceptions = [
-        {
-          type: "node",
-          check: {
-            ...errorMessages.errorConsoleLogNotInBody,
-            condition: (...args) =>
-              errorMessages.errorConsoleLogNotInBody.condition(...args, "Text"),
-          },
-          severity: "hint",
-          parseErrorCheck: "regular",
-        },
-      ]
-      it("with error", () => {
-        const code = `
+      const code = `
   if (true) {
+    console.log("Different")
+  }
+  console.log("Text")`
+      const errors = getDiagnostics(misconceptions, code)
+      assert.lengthOf(errors, 0)
+    })
+  })
+
+  describe("errorSwitchedCompareSymbol", () => {
+    const misconceptions = [
+      {
+        type: "node",
+        check: errorMessages.errorSwitchedCompareSymbol,
+        severity: "error",
+        parseErrorCheck: "both",
+      },
+    ]
+    it("with error", () => {
+      const code = `
+if (3 =< 5) {
+  console.log("Text")
+}`
+      const errors = getDiagnostics(misconceptions, code)
+      assert.lengthOf(errors, 1)
+      assert.deepEqual(errors[0].messages, [
+        "Das Gleichheitszeichen befindet sich bei Vergleichsoperatoren immer hinten.",
+        "Mögliche Vergleichsoperatoren sind <, >, <=, >=, != und ==.",
+        "Benutze >= 5, um 'Bestanden' anzuzeigen.",
+      ])
+    })
+    it("no error", () => {
+      const code = `
+  if (3 <= 5) {
     console.log("Different Text")
   }
   console.log("Text")`
-        const errors = getDiagnostics(misconceptions, code)
-        assert.lengthOf(errors, 0)
-      })
+      const errors = getDiagnostics(misconceptions, code)
+      assert.lengthOf(errors, 0)
+    })
+  })
+
+  describe("errorMissingIfElse", () => {
+    const misconceptions = [
+      {
+        type: "ast",
+        check: errorMessages.errorMissingIfElse,
+        severity: "hint",
+        parseErrorCheck: "regular",
+      },
+    ]
+    it("with error", () => {
+      const code = `
+if (3 < 5) {
+  console.log("Text")
+}`
+      const errors = getDiagnostics(misconceptions, code)
+      assert.lengthOf(errors, 1)
+      assert.deepEqual(errors[0].messages, [
+        "Diese Aufgabe benötigt eine if- und else-Anweisung. Sind beide Teil des Codes?",
+        `Die Syntax für eine if- und else-Anweisung sieht folgendermaßen aus:
+  if (kondition) {
+    //code
+  } else {
+    //code
+  }`,
+        `Ergänze if- und else:
+  if (kondition) {
+    //code
+  } else {
+    //code
+  }`,
+      ])
+    })
+    it("no error", () => {
+      const code = `
+if (3 < 5) {
+  console.log("Text")
+} else {
+  console.log("Different Text")
+}`
+      const errors = getDiagnostics(misconceptions, code)
+      assert.lengthOf(errors, 0)
+    })
+  })
+
+  describe("errorSemicolonAfterIfCondition", () => {
+    const misconceptions = [
+      {
+        type: "node",
+        check: errorMessages.errorSemicolonAfterIfCondition,
+        severity: "error",
+        parseErrorCheck: "regular",
+      },
+    ]
+    it("with error", () => {
+      const code = `
+if (3 < 5); {
+  console.log("Text")
+}`
+      const errors = getDiagnostics(misconceptions, code)
+      assert.lengthOf(errors, 1)
+      assert.deepEqual(errors[0].messages, [
+        "Achte auf die richtige Syntax bei der if-else-Anweisung.",
+        `Die Syntax für eine if- und else-Anweisung sieht folgendermaßen aus:
+  if (kondition) {
+    //code
+  } else {
+    //code
+  }`,
+        "Entferne das Semikolon nach den Klammern der if-Anweisung. Hier wird keine Methode aufgerufen, sondern ein neuer Block geöffnet. Dies passiert nicht mit dem Semikolon, sondern mit geschweiften Klammern nach der Bedingung.",
+      ])
+    })
+    it("no error", () => {
+      const code = `
+if (3 < 5) {
+  console.log("Text");
+}`
+      const errors = getDiagnostics(misconceptions, code)
+      assert.lengthOf(errors, 0)
     })
   })
 })
